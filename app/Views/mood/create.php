2720ae63aecdb3c4b40c7cd0034f3de431eb6ee5
TimW29 <timwijnandswerk@gmail.com>
2023-03-24 11:04:57 +0100

    Update create.php

--- app/Views/mood/create.php
+++ app/Views/mood/create.php
@@ -13,7 +13,19 @@
 
     <!-- Vul een mood in -->
     <label for="mood">Mood</label>
-    <textarea name="mood" cols="4" rows="1"><?= set_value('mood') ?></textarea>
+    <select name="mood">
+        <option <?= set_value('mood') ?>>0</option>
+        <option <?= set_value('mood') ?>>1</option>
+        <option <?= set_value('mood') ?>>2</option>
+        <option <?= set_value('mood') ?>>3</option>
+        <option <?= set_value('mood') ?>>4</option>
+        <option <?= set_value('mood') ?>>5</option>
+        <option <?= set_value('mood') ?>>6</option>
+        <option <?= set_value('mood') ?>>7</option>
+        <option <?= set_value('mood') ?>>8</option>
+        <option <?= set_value('mood') ?>>9</option>
+        <option <?= set_value('mood') ?>>10</option>
+    </select>
     <br>
 
     <!-- Vul een plek in -->
